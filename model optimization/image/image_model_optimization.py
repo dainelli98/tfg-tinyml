a28cab520b1ff598ffbdc2a65b5ce32e4f3681d3
daniel <danitiana98@gmail.com>
2021-05-13 17:34:38 +0200

    Modelo imagen pruning hecho.

--- model optimization/image/image_model_optimization.py
+++ model optimization/image/image_model_optimization.py
@@ -1,20 +1,35 @@
 import tensorflow_model_optimization as tfmot
 from tensorflow.keras.models import clone_model
 from tensorflow.keras import layers
-
-from image_model_training import get_image_model, get_image_datasets
+from typing import Any
 
 # Pruning
-PRUNING_EPOCHS: int = 500
+PRUNING_EPOCHS: int = 100
 PRUNABLE_LAYERS = [layers.ZeroPadding2D, layers.Conv2D, layers.BatchNormalization, layers.ReLU, layers.Flatten,
                    layers.Dense]
 
-PATIENCE: int = 20
+PATIENCE: int = 10
 
 
-def apply_prunning(model):
+def apply_pruning(model: Any) -> Any:
+    """
+    Aplica pruning a un modelo de tensorflow.
+    Args:
+        model:  Model de TensorFlow al que se aplica pruning.
 
-    def apply_pruning_to_layer(layer):
+    Returns:
+        Model de TensorFlow con pruning aplicado.
+    """
+
+    def apply_pruning_to_layer(layer: Any) -> Any:
+        """
+        Aplica low magnitude pruning a las capas compatibles.
+        Args:
+            layer:  Layer de TensorFlow a la que se le aplica pruning.
+
+        Returns:
+            Layer de TensorFlow a la que se ha aplicado pruning si es compatible.
+        """
         for prunable_layer in PRUNABLE_LAYERS:
             if isinstance(layer, prunable_layer):
                 return tfmot.sparsity.keras.prune_low_magnitude(layer)
